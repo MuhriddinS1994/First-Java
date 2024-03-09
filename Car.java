c6ef0ff28293b6cd75e021211d3523c1d86077e4
Muhriddin <muhriddin6064@gmail.com>
2024-03-10 01:05:01 +0500

    Encapsulation attributlar qo'shildi

--- Car.java
+++ Car.java
@@ -1,6 +1,21 @@
 package Core;
 // for GitHub example
 class Car {
- String color, model;
+ String color, model, transmission;
  int price;
+
+
+ public Car(String color, String model, String transmission, int price) {
+  this.color = color;
+  this.model = model;
+  this.transmission = transmission;
+  this.price = price;
+ }
+
+ public Car() {
+ }
+ public void print() {
+  System.out.println(model +" "+ color +" "+ transmission + " "+ price);
+ }
 }
+
