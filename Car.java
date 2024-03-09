e296610f1099b3d1e64bbc32c8cc08495a248ccc
Muhriddin <muhriddin6064@gmail.com>
2024-03-10 01:15:08 +0500

    Encapsulation prinsiplari qo'llandi

--- Car.java
+++ Car.java
@@ -5,17 +5,17 @@ class Car {
  int price;
 
 
-// public Car(String color, String model, String transmission, int price) {
-//  this.color = color;
-//  this.model = model;
-//  this.transmission = transmission;
-//  this.price = price;
-// }
-//
-// public Car() {
-// }
-// public void print() {
-//  System.out.println(model +" "+ color +" "+ transmission + " "+ price);
-// }
-//}
+ public Car(String color, String model, String transmission, int price) {
+  this.color = color;
+  this.model = model;
+  this.transmission = transmission;
+  this.price = price;
+ }
+
+ public Car() {
+ }
+ public void print() {
+  System.out.println(model +" "+ color +" "+ transmission + " "+ price);
+ }
+}
 
