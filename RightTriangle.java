533c10d7ab6794f57a8c220114a95bb31070949a
Muhriddin <muhriddin6064@gmail.com>
2024-03-10 01:43:49 +0500

    Encapsulatsiya prinsiplari qo'llandi, yuzasi va perimetrini hisoblovchi metodlar qo'shildi

--- RightTriangle.java
+++ RightTriangle.java
@@ -1,5 +1,26 @@
 package Core;
 // for GitHub test
 class RightTriangle {
-    int a,b,c;  // sides
+    private  double base;
+    private  double height;
+
+    public RightTriangle() {
+    }
+
+    public RightTriangle(double base, double height) {
+        if (base <=0 || height <=0) {
+            throw new IllegalArgumentException(" Base and height must be positive values.");
+        }
+        this.base = base;
+        this.height = height;
+    }
+
+    public double calculatePerimeter() {
+        double hypotenuse = Math.sqrt(base * base + height * height);
+        return base + height + hypotenuse;
+    }
+    public  double calculateArea() {
+        return 0.5*base*height;
+    }
+
 }
